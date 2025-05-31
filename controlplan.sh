ff8dda9c0c517c6e7745dc073d93104e43d0ddfd
Hitesh Kansagara <hiteshkansagara00@gmail.com>
2025-05-31 20:03:30 +0530

    script for controlplan node

---
+++ controlplan.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# Ensure script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "Please run as root or use sudo."
+   exit 1
+fi
+
+set -e
+
+echo "[INFO] permissiving selinux..."
+sed -i 's/SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
+
+echo "[INFO] Disabling swap..."
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+echo "Step 3 : Install Docker in machine "
+sudo dnf update -y
+sudo dnf install -y dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+ 
+systemctl start docker 
+systemctl enable docker
+
+echo "[INFO] Configuring containerd..."
+mkdir -p /etc/containerd
+containerd config default > /etc/containerd/config.toml
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+systemctl restart containerd
+systemctl enable containerd
+
+echo "[INFO] Loading kernel modules..."
+modprobe overlay
+modprobe br_netfilter
+
+tee /etc/sysctl.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sysctl --system
+
+echo "[INFO] Installing Kubernetes components..."
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/repodata/repomd.xml.key
+#exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
+
+echo "[INFO] Installing kubeadm kubelet kubectl..."
+yum install kubeadm kubelet kubectl -y
+
+echo "[INFO] Enable kubelet..."
+systemctl enable kubelet
+
+echo "Firewall rules"
+firewall-cmd --permanent --add-port=6443/tcp
+firewall-cmd --permanent --add-port=2379-2380/tcp
+firewall-cmd --permanent --add-port=10250/tcp
+firewall-cmd --permanent --add-port=10256/tcp
+firewall-cmd --permanent --add-port=10259/tcp
+firewall-cmd --permanent --add-port=10257/tcp
+firewall-cmd --permanent --add-port=30000-32767/tcp
+firewall-cmd --permanent --add-port=30000-32767/udp
+
+firewall-cmd --reload
+
+echo "[INFO] Initializing Kubernetes control plane..."
+POD_CIDR="10.244.0.0/16"
+kubeadm init --pod-network-cidr=$POD_CIDR --ignore-preflight-errors=all
+
+echo "[INFO] Configuring kubectl for current user..."
+mkdir -p $HOME/.kube
+cp /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+echo "[INFO] Tainting master node to allow scheduling pods..."
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+
+echo "[INFO] Installing Flannel CNI..."
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+
+echo "[✅ SUCCESS] Kubernetes single-node cluster is up and running!"
+kubectl get nodes
